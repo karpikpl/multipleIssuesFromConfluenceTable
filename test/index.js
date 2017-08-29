3ff2b6e4b40e7a78d485d89f71da5023b347390e
Piotr Karpala <pkarpala@infusion.com>
2017-08-28 20:57:59 -0400

    refactoring in progress

---
+++ test/index.js
@@ -0,0 +1,17 @@
+/*jshint esversion: 6, node: true*/
+'use strict';
+const Chai = require('chai');
+const should = Chai.should();
+
+const main = require('../index');
+
+describe('Index integration test', () => {
+
+    it('should read confluence page, create jira issues and update confluence with result', () => {
+
+        // act
+
+        // assert
+        true.should.be.false;
+    });
+});
